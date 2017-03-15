33816c384f7d54ea76623d5b0e4f34580d22ca50
audiotrash112@gmail.com <audiotrash112@gmail.com>
2017-03-15 19:06:44 -0400

    added Music Model

---
+++ audiopretzel/Controllers/MusicController.cs
@@ -0,0 +1,21 @@
+﻿using audiopretzel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace audiopretzel.Controllers
+{
+    public class MusicController : Controller
+    {
+        // GET: Music/Random
+        public ActionResult Random()
+        {
+            //the Path property inside Models.Path has to be exactly structured like : Path = "/Tunes/almost.mp3"
+            //inorder to be passed into the view
+            var music = new Music() { Name = "almost",Artist = "fasecity", Genre = "electronic",Path = "/Tunes/almost.mp3" };
+            return View(music);
+        }
+    }
+}
---
+++ audiopretzel/Models/Music.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace audiopretzel.Models
+{
+    public class Music
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Artist { get; set; }
+        public string Genre { get; set; }
+        //the Path property inside Models.Path has to be exactly structured like : Path = "/Tunes/almost.mp3"
+        //inorder to be passed into the feild in future
+        public string Path { get; set; }
+
+    }
+}
