300c014eb2840f912f364b7707ac81a808d69f69
audiotrash112@gmail.com <audiotrash112@gmail.com>
2017-03-15 23:07:40 -0400

    enabled migrations, added addmusic controller and addmusic view

--- audiopretzel/Controllers/MusicController.cs
+++ audiopretzel/Controllers/MusicController.cs
@@ -10,12 +10,29 @@ namespace audiopretzel.Controllers
     public class MusicController : Controller
     {
         // GET: Music/Random
+        /// <summary>
+        /// this method is going to eventually display a music table to client it uses Random view
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Random()
         {
+           
             //the Path property inside Models.Path has to be exactly structured like : Path = "/Tunes/almost.mp3"
             //inorder to be passed into the view
             var music = new Music() { Name = "almost",Artist = "fasecity", Genre = "electronic",Path = "/Tunes/almost.mp3" };
+
+            List<Music> musicList = new List<Music>();
+            musicList.Add(music);
             return View(music);
+            
+        }
+        /// <summary>
+        /// This Method is going to add music into database it uses AddMusic view for forms
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult AddMusic()
+        {
+            return View();
         }
     }
 }
